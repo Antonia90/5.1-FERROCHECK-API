d9ccdadbefcb3a8cac72a4081b8d358ae57c31c3
Antonia90 <antominnucci@gmail.com>
2025-09-21 20:23:33 +0200

    Fix duplicated email in local

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,32 +1,24 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-
-                User::factory()->admin()->create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('password'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Regular User',
-            'email' => 'user@example.com',
-            'password' => bcrypt('password'),
-        ]);
-
         if (app()->environment('local')) {
+            User::factory()->admin()->create([
+                'name' => 'Admin User',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('password'),
+            ]);
+
             User::factory()->create([
-                'name' => 'Test User',
-                'email' => 'test@example.com',
+                'name' => 'Regular User',
+                'email' => 'user@example.com',
+                'password' => bcrypt('password'),
             ]);
 
             $this->call(IngredientSeeder::class);
